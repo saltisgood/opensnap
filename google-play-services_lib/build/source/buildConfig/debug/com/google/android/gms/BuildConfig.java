97124e401b7f017dddd924505f2d16b462ec11af
Nicholas Stephen <git@nickstephen.com>
2014-02-10 22:18:17 +1100

    Added google-play-services_lib module

---
+++ google-play-services_lib/build/source/buildConfig/debug/com/google/android/gms/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.google.android.gms;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String PACKAGE_NAME = "com.google.android.gms";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 3265130;
+}
